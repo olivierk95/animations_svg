d9a82a53a423f219c868569769e8a70be0cb53e9
olivierk95 <keutgensolivier@gmail.com>
2019-04-08 10:39:52 +0200

    anime js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-import './Snap.svg/dist/snap.svg-min.js';
+import anime from './node_modules/animejs/lib/anime.es.js';
 
-var iconBelgium = {
-    selector:'.icon-belgium',
-    svg: '<svg xmlns="https:/www.w3.org/2000/svg" version="1.1" width="200" viewbox="0 0 3 2"><rect width="1" height="2" x="0" y="0" fill="#000" /><rect width="1" height="2" x="1" y="0" fill="#EC1" /><rect width="1" height="2" x="2" y="0" fill="#C23" /></svg>',
-};
-
-SnapStates(iconBelgium);
-
-console.log("niktamere");
+anime({
+    targets: '.linedrawing',
+    strokeDashoffset: [anime.setDashoffset],
+    easing: 'easeOutCubic',
+    duration: 5000,
+    delay: function(el, i) { return i * 0 },
+    direction: 'reverse'
+  });
